4d1370ed229c958a405b4815bb24f5cc5e662907
Rezmason <jeremysachs@rezmason.net>
2023-12-10 14:36:27 -0800

    SceneNode2D now has a recursive global transform recompose function.

--- js/scenenode2d.js
+++ js/scenenode2d.js
@@ -6,13 +6,16 @@ import { vec2 } from "./mathutils.js";
 const { mat2d } = glMatrix;
 
 export default class SceneNode2D extends SceneNode {
+	#globalMatrix = mat2d.create();
+	#invGlobalMatrix = mat2d.create();
+	#globalRotation = 0;
 	visible = true;
 	transform = new Transform2D();
 	colorTransform = new ColorTransform();
 	domElement = null;
 
 	get globalPosition() {
-		const matrix = this.#localToGlobalMatrix;
+		const matrix = this.globalMatrix;
 		return vec2.new(matrix[4], matrix[5]);
 	}
 
@@ -21,46 +24,42 @@ export default class SceneNode2D extends SceneNode {
 			this.transform.position = v;
 			return;
 		}
-
-		const g2l = this.parent.#localToGlobalMatrix;
-		mat2d.invert(g2l, g2l);
-
+		const pm = this.parent.invGlobalMatrix;
 		this.transform.position = vec2.new(
-			v[0] * g2l[0] + v[1] * g2l[2] + g2l[4],
-			v[0] * g2l[1] + v[1] * g2l[3] + g2l[5]
+			v[0] * pm[0] + v[1] * pm[2] + pm[4],
+			v[0] * pm[1] + v[1] * pm[3] + pm[5]
 		);
 	}
 
-	get #localToGlobalMatrix() {
-		let node = this;
-		const matrix = mat2d.create();
-		while (node != null) {
-			mat2d.multiply(matrix, node.transform.matrix, matrix);
-			node = node.parent;
-		}
-		return matrix;
+	get globalMatrix() {
+		this.#recomposeGlobalMatrix();
+		return this.#globalMatrix;
 	}
 
-	// These cheat— angles are preserved for uniform scales,
-	// and all scales in this project are uniform
+	get invGlobalMatrix() {
+		this.#recomposeGlobalMatrix();
+		return this.#invGlobalMatrix;
+	}
 
 	get globalRotation() {
-		return this.#localToGlobalRotation;
+		this.#recomposeGlobalMatrix();
+		return this.#globalRotation;
 	}
 
 	set globalRotation(v) {
-		const globalRotation = this.#localToGlobalRotation;
+		const globalRotation = this.globalRotation;
 		const diff = v - globalRotation;
 		this.transform.rotation += diff;
 	}
 
-	get #localToGlobalRotation() {
-		let node = this;
-		let globalRotation = 0;
-		while (node != null) {
-			globalRotation += node.transform.rotation;
-			node = node.parent;
+	#recomposeGlobalMatrix() {
+		this.#globalRotation = this.transform.rotation;
+		const matrix = this.#globalMatrix;
+		mat2d.copy(matrix, this.transform.matrix);
+		if (this.parent != null) {
+			this.#globalRotation += this.parent.globalRotation;
+			mat2d.multiply(matrix, this.parent.globalMatrix, matrix);
 		}
-		return globalRotation;
+		mat2d.invert(this.#invGlobalMatrix, matrix);
 	}
 }
